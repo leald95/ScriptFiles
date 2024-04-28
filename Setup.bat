a94b7fd47a170cc8197c34c19a14a64c32c02cd3
leald95 <9266924+leald95@users.noreply.github.com>
2024-04-28 19:52:25 -0400

    Update Setup.bat

--- Setup.bat
+++ Setup.bat
@@ -1,1 +1,1 @@
-"%~dp0VMware-Horizon-Client-2312-8.12.0-23149293.exe" /silent VDM_Server=ccaccess.sehp.stelizabeth.com ADDLOCAL=USB URL_FILTERING_ENABLED=1 AUTO_UPDATE_ENABLED=0 DESKTOP_SHORTCUT=1 /norestart
+"%~dp0VMwareHorizons.exe" /silent VDM_Server=ccaccess.sehp.stelizabeth.com ADDLOCAL=USB URL_FILTERING_ENABLED=1 AUTO_UPDATE_ENABLED=0 DESKTOP_SHORTCUT=1 /norestart
